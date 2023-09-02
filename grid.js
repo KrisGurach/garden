391755ecf45a38eea59c7a588a998aa25ae055c2
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-09-02 15:51:38 +0600

    added .env

--- grid.js
+++ grid.js
@@ -2,5 +2,4 @@ document.querySelector('.form__toggle').onclick = () => {
     document.querySelector('.form').classList.toggle('form_is-closed');
   }
 
-let  test = document.querySelector('food__cell');
-let antitest = document.querySelector('food__description');
+
